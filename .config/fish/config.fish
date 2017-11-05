b31d2386761c41440434fe6ab0acc359d27f213c
Mark Waddle <mark@markwaddle.com>
2017-11-05 09:54:18 -0800

    Adds fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+source ~/.fish_aliases
+source ~/.fish_variables
+set -g theme_nerd_fonts yes
+set -g theme_show_exit_status yes
+
+set -g VIRTUAL_ENV_DISABLE_PROMPT 1
+source ~/virtualenv/py2/bin/activate.fish
